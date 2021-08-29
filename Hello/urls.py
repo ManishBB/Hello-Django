34994d6aca73ae8ae8c157288bb30d51aa6a999e
Manish Bhamare <mbb7224@gmail.com>
2021-08-29 11:05:48 +0530

    Added Templates & Static data

--- Hello/urls.py
+++ Hello/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+admin.site.site_header = "Manish Ice Cream Admin"
+admin.site.site_title = "Manish Ice Cream Admin Portal"
+admin.site.index_title = "Welcome to  Manish Ice Creams"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include('Home.urls'))
 ]
---
+++ Home/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def index(request):
+    context = {
+        'variable' : 'Used variable!'
+    }
+    return render(request , "index.html" )
+
+def about(request):
+    return render(request , "about.html" )
+    # return HttpResponse("This is about page")
+
+def services(request):
+    return render(request , "services.html" )
+    # return HttpResponse("This is services page")
+
+def contact(request):
+    return render(request , "contact.html" )
+    # return HttpResponse("This is contact page")
